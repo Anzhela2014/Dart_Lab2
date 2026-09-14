dcdc863614412f90fc35d0572c777033334629ae
Konovalov Alexander <yhy60249@gmail.com>
2026-09-14 13:38:12 +0300

    test

--- bin/lare2.dart
+++ bin/lare2.dart
@@ -1,27 +1,15 @@
-
 import 'package:lare2/todo.dart';
-void main(){
-// Todo task = Todo(1, 'Купить продукты');
-// print(task.id);
-// print(task.title);
-// print(task.isDone);
+import 'dart:io';
 
+void main() {
+//   Todo task1 = Todo(id: 1, title: 'Купить продукты');
+//   Todo task2 = Todo(id: 2, title: 'Сделать зарядку');
+//   task1.complete();
+//   print(task1);
+//   print(task2);
+// }
 
-// Todo task = Todo(
-//     id: 1,
-//     title: 'Купить продукты',
-
-  
-  // );
- void main() {
-  Todo task1 = Todo(id: 1, title: 'Купить продукты');
-  Todo task2 = Todo(id: 2, title: 'Сделать зарядку');
-  task1.complete();
-  print(task1);
-  print(task2);
+  stdout.write('Введите что-нибудь: ');
+  String? input = stdin.readLineSync();
+  print('Вы ввели: $input');
 }
-
-
-}
-
-
--- lib/todo.dart
+++ lib/todo.dart
@@ -1,23 +1,25 @@
 class Todo {
-//   int id;
-//   String title;
-//   bool isDone;
-// // Todo(this.id, this.title) : isDone = false;
-// Todo({required this.id, required this.title})
-// : isDone = false;
-//  @override
-// String toString() {
-//   String status = isDone ? '[x]' : '[ ]';
-//   return '$status $id. $title';
-// }
- void complete() {
-  isDone = true;
+  int id;
+  String title;
+  bool isDone;
+// Todo(this.id, this.title) : isDone = false;
+Todo({required this.id, required this.title})
+: isDone = false;
 
+void complete() {
+  isDone = true;
+}
+
+ @override
+String toString() {
+  String status = isDone ? '[x]' : '[ ]';
+  return '$status $id. $title';
+}
   // Todo(int id, String title) {
   // this.id = id;
   // this.title = title;
   // this.isDone = false;
- }
+  
 // }
 
 // int? id;
