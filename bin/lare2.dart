40536237c9cdeda25e9c66761407f420531a9178
Konovalov Alexander <yhy60249@gmail.com>
2026-09-14 12:23:41 +0300

    Initial dart project

---
+++ bin/lare2.dart
@@ -0,0 +1,5 @@
+import 'package:lare2/lare2.dart' as lare2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lare2.calculate()}!');
+}
---
+++ test/lare2_test.dart
@@ -0,0 +1,8 @@
+import 'package:lare2/lare2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
